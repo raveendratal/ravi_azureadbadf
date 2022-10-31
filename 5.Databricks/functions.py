4d2bf5edb72666a3f1eeff0df5f4202b9aee4e4c
raveendratal <raveendratal>
2022-10-31 15:31:25 +0000

    removed new notebooks

--- 5.Databricks/functions.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-def add(a,b):
-    c=a+b
-    return c
--- 5.Databricks/libraries.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-import pandas as pd
