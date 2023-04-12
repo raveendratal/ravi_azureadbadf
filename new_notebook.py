74dd533943c37553de5daeee4264c6ca1e8d4f28
raveendratal <raveendratal>
2023-04-12 02:44:32 +0000

    new changes

---
+++ new_notebook.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC show databases;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC use gold_ticket_db;
+# MAGIC show tables;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from listings_fact
+
+# COMMAND ----------
+
+
